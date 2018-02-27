9e5cc61112fa258c8acf2c981ba5e60ed43f021d
Bruno Dutra <brunocodutra@gmail.com>
2018-02-27 21:55:44 +0100

    better protect equal from edge cases

--- spec/equal.spec.ts
+++ spec/equal.spec.ts
@@ -1,13 +1,20 @@
-import {equal, polar, rect} from 'index';
+import {equal, polar} from 'index';
 
 import {samples} from './util';
 
 describe('Complex', () => {
   it('should be comparable for equality', () => {
-    samples.forEach(({real, imag, mag, ang}) => {
-      expect(equal(polar(mag, ang), rect(real, imag), 1E-15)).toBe(true);
+    samples.forEach(({ mag, ang}) => {
+      expect(equal(polar(0, ang), polar(0))).toBe(true);
+
+      expect(equal(polar(mag, ang - Math.PI), polar(-mag, ang), 1E-6)).toBe(true);
+      expect(equal(polar(mag, ang - Math.PI / 2), polar(-mag, ang), 1E-6)).toBe(!mag);
+      expect(equal(polar(mag, ang + Math.PI / 2), polar(-mag, ang), 1E-6)).toBe(!mag);
+      expect(equal(polar(mag, ang + Math.PI), polar(-mag, ang), 1E-6)).toBe(true);
     });
 
-    expect(equal(polar(NaN), rect(NaN))).toEqual(false);
+    expect(equal(polar(NaN), polar(NaN))).toEqual(false);
+    expect(equal(polar(0, NaN), polar(0, NaN))).toEqual(false);
+    expect(equal(polar(NaN, NaN), polar(NaN, NaN))).toEqual(false);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -35,27 +35,75 @@ export const rect = (re: number, im = 0): Complex => complex(
 
 export const i = (im = 1) => rect(0, im);
 
+const cosatan = (x: number) => 1 / Math.hypot(1, x);
+const sinatan = (x: number) => Math.sign(x) * cosatan(1 / x);
+
+const tanaddatan = (b: number, d: number) => {
+  const absb = Math.abs(b);
+  const absd = Math.abs(d);
+
+  return (
+      (absb === 0)
+    ? d
+    : (absd === 0)
+    ? b
+    : (absb <= 1 && absd <= 1)
+    ? (b + d) / (1 - b * d)
+    : (absb > 1 && absd <= 1)
+    ? (1 + d / b) / (1 / b - d)
+    : (absb <= 1 && absd > 1)
+    ? (b / d + 1) / (1 / d - b)
+    : (1 / d + 1 / b) / ((1 / b) * (1 / d) - 1)
+  );
+};
+
+const tansubatan = (b: number, d: number) => {
+  const absb = Math.abs(b);
+  const absd = Math.abs(d);
+
+  return (
+      (absb === 0)
+    ? -d
+    : (absd === 0)
+    ? b
+    : (absb <= 1 && absd <= 1)
+    ? (b - d) / (1 + b * d)
+    : (absb > 1 && absd <= 1)
+    ? (1 - d / b) / (1 / b + d)
+    : (absb <= 1 && absd > 1)
+    ? (b / d - 1) / (1 / d + b)
+    : (1 / d - 1 / b) / ((1 / b) * (1 / d) + 1)
+  );
+};
+
 const close = (x: number, y: number, e: number) => (
-  (x === y) || (Math.abs(x - y) < e) || (Math.abs(x - y) / Math.hypot(x, y)) < e
+  (x === y) ||
+  (Math.abs(x - y) <= Math.abs(e) ** 1.5) ||
+  (Math.abs(x - y) / Math.hypot(x, y)) <= Math.abs(e) / 2 ** 0.5
 );
 
-const cosatan = (x: number) => 1 / Math.hypot(1, x);
+export const equal = (p: Complex, q: Complex, e = 0) => {
+  const {mag: a, tan: b} = p;
+  const {mag: c, tan: d} = q;
 
-export const equal = (p: Complex, q: Complex, e = 0) => (
-  close(Math.abs(p.mag), Math.abs(q.mag), e) &&
-  close(cosatan(p.tan) * cosatan(q.tan) + cosatan(1 / p.tan) * cosatan(1 / q.tan), 1, e)
-) || (
-  close(p.mag * cosatan(p.tan), q.mag * cosatan(q.tan), e) &&
-  close(p.mag * cosatan(1 / p.tan), q.mag * cosatan(1 / q.tan), e)
-)
+  return (
+      (close(b, 0, e) && close(d, 0, e))
+    ? close(a, c, e)
+    : (close(1 / b, 0, e) && close(1 / d, 0, e))
+    ? close(a * Math.sign(b), c * Math.sign(d), e)
+    : (Math.abs(a) == Infinity && Math.abs(c) == Infinity)
+    ? close(sinatan(b) * sinatan(d) + cosatan(b) * cosatan(d), Math.sign(a * c), e ** 2)
+    : close(a ** 2 + c ** 2, 2 * a * c * cosatan(tansubatan(b, d)), e ** 2)
+  );
+};
 
 export const norm = (p: Complex): number => Math.abs(p.mag);
 export const angle = (p: Complex): number => Math.atan(p.tan) + (
   (p.mag < 0) ? (p.tan > 0) ? -Math.PI : Math.PI : 0
 );
 
-export const real = (p: Complex): number => (1 / p.tan) && (p.mag / Math.hypot(1, p.tan));
-export const imag = (p: Complex): number => Math.sign(p.tan) * real({mag: p.mag, tan: 1 / p.tan});
+export const real = (p: Complex): number => (1 / p.tan) && p.mag * cosatan(p.tan);
+export const imag = (p: Complex): number => p.tan && p.mag * sinatan(p.tan);
 
 export const neg = (p: Complex): Complex => complex(-p.mag, p.tan);
 export const conj = (p: Complex): Complex => complex(p.mag, -p.tan);
@@ -72,22 +120,7 @@ export const mul = (p: Complex, q: Complex): Complex => {
   const {mag: a, tan: b} = p;
   const {mag: c, tan: d} = q;
 
-  const absb = Math.abs(b);
-  const absd = Math.abs(d);
-
-  const tan = (
-      (absb === 0)
-    ? d
-    : (absd === 0)
-    ? b
-    : (absb <= 1 && absd <= 1)
-    ? (b + d) / (1 - b * d)
-    : (absb > 1 && absd <= 1)
-    ? (1 + d / b) / (1 / b - d)
-    : (absb <= 1 && absd > 1)
-    ? (b / d + 1) / (1 / d - b)
-    : (1 / d + 1 / b) / ((1 / b) * (1 / d) - 1)
-  );
+  const tan = tanaddatan(b, d);
 
   const mag = a * c * ((
     (b > 0 && d > 0 && tan <= 0) ||
@@ -101,22 +134,7 @@ export const div = (p: Complex, q: Complex): Complex => {
   const {mag: a, tan: b} = p;
   const {mag: c, tan: d} = q;
 
-  const absb = Math.abs(b);
-  const absd = Math.abs(d);
-
-  const tan = (
-      (absb === 0)
-    ? -d
-    : (absd === 0)
-    ? b
-    : (absb <= 1 && absd <= 1)
-    ? (b - d) / (1 + b * d)
-    : (absb > 1 && absd <= 1)
-    ? (1 - d / b) / (1 / b + d)
-    : (absb <= 1 && absd > 1)
-    ? (b / d - 1) / (1 / d + b)
-    : (1 / d - 1 / b) / ((1 / b) * (1 / d) + 1)
-  );
+  const tan = tansubatan(b, d);
 
   const mag = a / c * ((
     (b > 0 && d < 0 && tan <= 0) ||
