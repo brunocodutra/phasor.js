7300a6ff5e59f99fba17d4935775f0c03220d05e
Bruno Dutra <brunocodutra@gmail.com>
2018-12-08 17:06:46 +0100

    fix missing type definitions

--- spec/util.ts
+++ spec/util.ts
@@ -1,8 +1,8 @@
 type Sample = {mag: number, ang: number, real: number, imag: number};
 
-export const samples: Sample[] = [].concat.apply(
-  [{mag: 0, ang: 0, real: 0, imag: 0}],
-  [1E-6, 1E-3, 1, 1E3, 1E6, Infinity].map((mag) =>
+export const samples: Sample[] = [
+  {mag: 0, ang: 0, real: 0, imag: 0},
+  ...[1E-6, 1E-3, 1, 1E3, 1E6, Infinity].map((mag) =>
     Array(33).fill(Math.PI / 8).map((p, k) => {
       const ang = p * k - 2 * Math.PI;
       const cos = Math.cos(ang % (Math.PI * 2));
@@ -13,5 +13,5 @@ export const samples: Sample[] = [].concat.apply(
 
       return {mag, ang, real, imag};
     }),
-  ),
-);
+  ).flat(),
+];
