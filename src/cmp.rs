2bee178a7b47e16735b085fe3d138a730b7ae1a3
Bruno Dutra <brunocodutra@gmail.com>
2020-08-22 20:32:30 +0200

    add assertion macro to compare floats

---
+++ src/cmp.rs
@@ -0,0 +1,23 @@
+#[macro_export]
+macro_rules! assert_close_to {
+    ($lhs:expr, $rhs:expr) => {
+        assert_close_to!($lhs, $rhs, tol = 1E-15)
+    };
+
+    ($lhs:expr, $rhs:expr, tol = $tol:expr) => {
+        assert!(
+            ::approx::RelativeEq::relative_eq(&$lhs, &$rhs, $tol, $tol)
+                || ::approx::UlpsEq::ulps_eq(&$lhs, &$rhs, $tol, ($tol / f64::EPSILON) as u32),
+            r#"expected `lhs` to be approximately equal to `rhs`:
+    lhs: `{:e} = {}`,
+    rhs: `{:e} = {}`
+    tol: `{:e}`
+"#,
+            $lhs,
+            stringify!($lhs),
+            $rhs,
+            stringify!($rhs),
+            $tol
+        )
+    };
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[cfg(test)]
+mod cmp;
