9ae892200c25195c42a3f89f078d51f84b3c8581
Bruno Dutra <brunocodutra@gmail.com>
2020-08-27 23:58:00 +0200

    impl Div for Phasor

---
+++ src/phasor/div.rs
@@ -0,0 +1,43 @@
+use super::*;
+use std::ops::Div;
+
+impl Div for Phasor {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        let tan = tansubatan(self.tan, rhs.tan);
+        let mag = if self.tan.signum() != tan.signum() && rhs.tan.signum() == tan.signum() {
+            -self.mag / rhs.mag
+        } else {
+            self.mag / rhs.mag
+        };
+
+        Phasor { mag, tan }
+    }
+}
+
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod tests {
+    use super::*;
+    use crate::assert_close_to;
+    use proptest::prelude::*;
+
+    proptest! {
+        #[test]
+        fn div(a: f64, b: f64, c: f64, d: f64) {
+            let p = Phasor::rect(a, b);
+            let q = Phasor::rect(c, d);
+
+            let t = p.mag.abs().max(1f64);
+            let u = q.mag.abs();
+
+            let r = Phasor::rect(
+                t  * ((a / t) * (c / u) + (b / t) * (d / u)) / u,
+                t  * ((b / t) * (c / u) - (a / t) * (d / u)) / u,
+            );
+
+            prop_assume!(r.mag.is_normal());
+            assert_close_to!(p / q, r);
+        }
+    }
+}
