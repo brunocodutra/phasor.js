5f15a323021631b256aabb873b22fcdf29d21af9
Bruno Dutra <brunocodutra@gmail.com>
2020-09-19 18:22:45 +0200

    cover edge cases in real() and imag()

--- src/phasor/imag.rs
+++ src/phasor/imag.rs
@@ -7,23 +7,51 @@ use wasm_bindgen::prelude::*;
 impl Phasor {
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
     pub fn imag(&self) -> f64 {
-        self.mag * sinatan(self.tan)
+        let s = sinatan(self.tan);
+        if s.abs() > 0f64 {
+            s * self.mag
+        } else {
+            s * self.mag.signum()
+        }
     }
 }
 
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
     use super::*;
+    use crate::arbitrary::{any, *};
     use crate::assert_close_to;
-    use alloc::format;
     use proptest::prelude::*;
 
     proptest! {
         #[test]
-        fn imag(re: f64, im: f64) {
-            let p = Phasor::rect(re as f64, im as f64);
-            prop_assume!(p.real().is_normal());
-            assert_close_to!(p.imag(), im as f64);
+        fn imaginary_part_of_finite_phasor_equals_magnitude_times_sine_of_angle(mag in finite(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            assert_close_to!(p.imag(), mag * sinatan(tan));
+        }
+
+        #[test]
+        fn imaginary_part_of_real_phasor_is_zero(mag in not_nan(), tan in zero()) {
+            let p = Phasor { mag, tan };
+            assert_close_to!(p.imag(), 0f64);
+        }
+
+        #[test]
+        fn imaginary_part_of_imaginary_phasor_equals_magnitude(mag in not_nan(), tan in infinite()) {
+            let p = Phasor { mag, tan };
+            assert_close_to!(p.imag(), mag * tan.signum());
+        }
+
+        #[test]
+        fn imaginary_part_of_phasor_with_undefined_magnitude_is_nan(mag in nan(), tan in any()) {
+            let p = Phasor { mag, tan };
+            assert!(p.imag().is_nan());
+        }
+
+        #[test]
+        fn imaginary_part_of_phasor_with_undefined_tangent_is_nan(mag in any(), tan in nan()) {
+            let p = Phasor { mag, tan };
+            assert!(p.imag().is_nan());
         }
     }
 }
