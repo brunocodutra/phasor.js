33e847277705e92fe3ef413320438f94ef654367
Bruno Dutra <brunocodutra@gmail.com>
2020-09-19 18:22:45 +0200

    cover edge cases in real() and imag()

--- src/phasor/real.rs
+++ src/phasor/real.rs
@@ -7,23 +7,51 @@ use wasm_bindgen::prelude::*;
 impl Phasor {
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
     pub fn real(&self) -> f64 {
-        self.mag * cosatan(self.tan)
+        let c = cosatan(self.tan);
+        if c.abs() > 0f64 {
+            c * self.mag
+        } else {
+            c * self.mag.signum()
+        }
     }
 }
 
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
     use super::*;
+    use crate::arbitrary::{any, *};
     use crate::assert_close_to;
-    use alloc::format;
     use proptest::prelude::*;
 
     proptest! {
         #[test]
-        fn real(re: f64, im: f64) {
-            let p = Phasor::rect(re as f64, im as f64);
-            prop_assume!(p.real().is_normal());
-            assert_close_to!(p.real(), re as f64);
+        fn real_part_of_finite_phasor_equals_magnitude_times_cosine_of_angle(mag in finite(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            assert_close_to!(p.real(), mag * cosatan(tan));
+        }
+
+        #[test]
+        fn real_part_of_real_phasor_equals_magnitude(mag in not_nan(), tan in zero()) {
+            let p = Phasor { mag, tan };
+            assert_close_to!(p.real(), mag);
+        }
+
+        #[test]
+        fn real_part_of_imaginary_phasor_is_zero(mag in not_nan(), tan in infinite()) {
+            let p = Phasor { mag, tan };
+            assert_close_to!(p.real(), 0f64);
+        }
+
+        #[test]
+        fn real_part_of_phasor_with_undefined_magnitude_is_nan(mag in nan(), tan in any()) {
+            let p = Phasor { mag, tan };
+            assert!(p.real().is_nan());
+        }
+
+        #[test]
+        fn real_part_of_phasor_with_undefined_tangent_is_nan(mag in any(), tan in nan()) {
+            let p = Phasor { mag, tan };
+            assert!(p.real().is_nan());
         }
     }
 }
