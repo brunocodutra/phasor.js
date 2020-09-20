brunocodutra/phasor.js

commit 308bc50d2879b6a5fb38a874b03afc0a8d9e942e

make approximated equality stricter

src/phasor/approx.rs
@@ -1,11 +1,14 @@
 use super::*;
 use ::approx::{AbsDiffEq, RelativeEq, UlpsEq};
 
-fn distance(p: &Phasor, q: &Phasor) -> (f64, f64) {
-    (
-        (p.mag / 2f64).hypot(q.mag / 2f64),
-        cosatan(tansubatan(p.tan, q.tan)) / p.mag.recip().hypot(q.mag.recip()),
-    )
+fn distance(p: &Phasor, q: &Phasor) -> f64 {
+    if (p.mag.is_infinite() && q.mag.is_infinite())
+        || (p.mag.recip().is_infinite() && q.mag.recip().is_infinite())
+    {
+        cosatan(tansubatan(p.tan, q.tan))
+    } else {
+        2f64 * cosatan(tansubatan(p.tan, q.tan)) * cosatan(p.mag / q.mag) * cosatan(q.mag / p.mag)
+    }
 }
 
 impl AbsDiffEq for Phasor {
@@ -16,8 +19,7 @@ impl AbsDiffEq for Phasor {
     }
 
     fn abs_diff_eq(&self, other: &Self, e: Self::Epsilon) -> bool {
-        let (x, y) = distance(self, other);
-        x.abs_diff_eq(&y, e)
+        distance(self, other).abs_diff_eq(&1f64, e)
     }
 }
 
@@ -27,8 +29,7 @@ impl RelativeEq for Phasor {
     }
 
     fn relative_eq(&self, other: &Self, e: Self::Epsilon, max: Self::Epsilon) -> bool {
-        let (x, y) = distance(self, other);
-        x.relative_eq(&y, e, max)
+        distance(self, other).relative_eq(&1f64, e, max)
     }
 }
 
@@ -38,8 +39,7 @@ impl UlpsEq for Phasor {
     }
 
     fn ulps_eq(&self, other: &Self, e: Self::Epsilon, max: u32) -> bool {
-        let (x, y) = distance(self, other);
-        x.ulps_eq(&y, e, max)
+        distance(self, other).ulps_eq(&1f64, e, max)
     }
 }
 
@@ -51,13 +51,11 @@ mod tests {
 
     proptest! {
         #[test]
-        fn close_to(p: Phasor) {
-            let q = Phasor {
-                mag: p.mag + f64::EPSILON,
-                tan: p.tan + f64::EPSILON,
-            };
-
-            assert_close_to!(p, q);
+        fn close_to(a: f64, b: f64) {
+            assert_close_to!(
+                Phasor::rect(a * 1.0000000001, b * 0.9999999999),
+                Phasor::rect(a * 0.9999999999, b * 1.0000000001)
+            );
         }
     }
 }

commit 325b454732290a0150926a07e958db42db46434c

fix regression introduced in 308bc50
special care needs to be taken when comparing phasors whose arguments are close to +-PI/2

src/arbitrary.rs
@@ -1,4 +1,4 @@
-use proptest::num::f64::*;
+use proptest::{num::f64::*, prelude::*};
 
 pub(crate) fn any() -> Any {
     ANY
@@ -37,4 +37,8 @@ pub(crate) fn nonzero() -> Any {
 
 pub(crate) fn regular() -> Any {
     normal() | subnormal()
+}
+
+pub(crate) fn modulo(period: f64) -> impl Strategy<Value = f64> {
+    finite().prop_map(move |n| n % period).boxed()
 }

src/phasor/approx.rs
@@ -1,17 +1,15 @@
 use super::*;
 use ::approx::{AbsDiffEq, RelativeEq, UlpsEq};
 
-fn distance(p: &Phasor, q: &Phasor) -> f64 {
-    use core::num::FpCategory::*;
+#[allow(clippy::float_cmp)]
+fn similarity(p: &Phasor, q: &Phasor) -> f64 {
+    let k = if p.mag.abs() == q.mag.abs() {
+        p.mag.signum() * q.mag.signum()
+    } else {
+        sinatan2(p.mag, q.mag) * cosatan2(p.mag, q.mag) * 2f64
+    };
 
-    match (p.classify(), q.classify()) {
-        (Zero, Zero) | (Infinite, Infinite) => cosatan(tansubatan(p.tan, q.tan)),
-        _ => {
-            2f64 * cosatan(tansubatan(p.tan, q.tan))
-                * cosatan(p.mag / q.mag)
-                * cosatan(q.mag / p.mag)
-        }
-    }
+    k * cossubatan(p.tan, q.tan)
 }
 
 impl AbsDiffEq for Phasor {
@@ -22,7 +20,7 @@ impl AbsDiffEq for Phasor {
     }
 
     fn abs_diff_eq(&self, other: &Self, e: Self::Epsilon) -> bool {
-        distance(self, other).abs_diff_eq(&1f64, e)
+        similarity(self, other).abs_diff_eq(&1f64, e)
     }
 }
 
@@ -32,7 +30,7 @@ impl RelativeEq for Phasor {
     }
 
     fn relative_eq(&self, other: &Self, e: Self::Epsilon, max: Self::Epsilon) -> bool {
-        distance(self, other).relative_eq(&1f64, e, max)
+        similarity(self, other).relative_eq(&1f64, e, max)
     }
 }
 
@@ -42,62 +40,211 @@ impl UlpsEq for Phasor {
     }
 
     fn ulps_eq(&self, other: &Self, e: Self::Epsilon, max: u32) -> bool {
-        distance(self, other).ulps_eq(&1f64, e, max)
+        similarity(self, other).ulps_eq(&1f64, e, max)
     }
 }
 
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
     use super::*;
-    use proptest::{num::f64::NORMAL, prelude::*};
+    use crate::arbitrary::*;
+    use core::f64::EPSILON;
+    use proptest::prelude::*;
 
     proptest! {
         #[test]
-        fn abs_diff_eq(a: f64, b: f64) {
-            let p = Phasor::rect(a * 1.000001, b * 0.999999);
-            let q = Phasor::rect(a * 0.999999, b * 1.000001);
+        fn abs_diff_eq_nan(nan in nan(), not_nan in not_nan()) {
+            let p = Phasor { mag: nan, tan: nan };
+            assert!(!p.abs_diff_eq(&p, 1f64));
 
-            assert!(p.abs_diff_eq(&q, 1E-10));
+            let p = Phasor { mag: nan, tan: not_nan };
+            assert!(!p.abs_diff_eq(&p, 1f64));
+
+            let p = Phasor { mag: not_nan, tan: nan };
+            assert!(!p.abs_diff_eq(&p, 1f64));
         }
 
         #[test]
-        fn abs_diff_neq(a in NORMAL, b in NORMAL) {
-            let p = Phasor::rect(a * 1.000001, b * 0.999999);
-            let q = Phasor::rect(a * 0.999999, b * 1.000001);
+        fn abs_diff_eq_same(mag in not_nan(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            assert!(p.abs_diff_eq(&p, EPSILON));
+        }
 
+        #[test]
+        fn abs_diff_eq_real(mag in nonzero(), tan in zero()) {
+            let p = Phasor { mag, tan };
+            assert!(p.abs_diff_eq(&p, EPSILON));
+
+            let q = Phasor { mag, tan: -tan };
+            assert!(p.abs_diff_eq(&q, EPSILON));
+
+            let q = Phasor { mag: -mag, tan };
+            assert!(!p.abs_diff_eq(&q, 1f64));
+
+            let q = Phasor { mag: -mag, tan: -tan };
+            assert!(!p.abs_diff_eq(&q, 1f64));
+        }
+
+        #[test]
+        fn abs_diff_eq_imaginary(mag in nonzero(), tan in infinite()) {
+            let p = Phasor { mag, tan };
+            assert!(p.abs_diff_eq(&p, EPSILON));
+
+            let q = Phasor { mag, tan: -tan };
+            assert!(!p.abs_diff_eq(&q, 1f64));
+
+            let q = Phasor { mag: -mag, tan };
+            assert!(!p.abs_diff_eq(&q, 1f64));
+
+            let q = Phasor { mag: -mag, tan: -tan };
+            assert!(p.abs_diff_eq(&q, EPSILON));
+        }
+
+        #[test]
+        fn abs_diff_eq_scaled(mag in regular(), tan in not_nan()) {
+            let p = Phasor { mag: mag * 1.000001, tan };
+            let q = Phasor { mag: mag / 1.000001, tan };
+
+            assert!(p.abs_diff_eq(&q, 1E-10));
             assert!(!p.abs_diff_eq(&q, 1E-12));
         }
 
         #[test]
-        fn relative_eq(a: f64, b: f64) {
-            let p = Phasor::rect(a * 1.000001, b * 0.999999);
-            let q = Phasor::rect(a * 0.999999, b * 1.000001);
+        fn abs_diff_eq_opposite(mag in nonzero(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            let q = Phasor { mag: -mag, tan };
 
-            assert!(p.relative_eq(&q, 0f64, 1E-10));
+            assert!(!p.abs_diff_eq(&q, 1f64));
         }
 
         #[test]
-        fn relative_neq(a in NORMAL, b in NORMAL) {
-            let p = Phasor::rect(a * 1.000001, b * 0.999999);
-            let q = Phasor::rect(a * 0.999999, b * 1.000001);
+        fn relative_eq_nan(nan in nan(), not_nan in not_nan()) {
+            let p = Phasor { mag: nan, tan: nan };
+            assert!(!p.relative_eq(&p, 0f64, 1f64));
 
+            let p = Phasor { mag: nan, tan: not_nan };
+            assert!(!p.relative_eq(&p, 0f64, 1f64));
+
+            let p = Phasor { mag: not_nan, tan: nan };
+            assert!(!p.relative_eq(&p, 0f64, 1f64));
+        }
+
+        #[test]
+        fn relative_eq_same(mag in not_nan(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            assert!(p.relative_eq(&p, 0f64, EPSILON));
+        }
+
+        #[test]
+        fn relative_eq_real(mag in nonzero(), tan in zero()) {
+            let p = Phasor { mag, tan };
+            assert!(p.relative_eq(&p, 0f64, EPSILON));
+
+            let q = Phasor { mag, tan: -tan };
+            assert!(p.relative_eq(&q, 0f64, EPSILON));
+
+            let q = Phasor { mag: -mag, tan };
+            assert!(!p.relative_eq(&q, 0f64, 1f64));
+
+            let q = Phasor { mag: -mag, tan: -tan };
+            assert!(!p.relative_eq(&q, 0f64, 1f64));
+        }
+
+        #[test]
+        fn relative_eq_imaginary(mag in nonzero(), tan in infinite()) {
+            let p = Phasor { mag, tan };
+            assert!(p.relative_eq(&p, 0f64, EPSILON));
+
+            let q = Phasor { mag, tan: -tan };
+            assert!(!p.relative_eq(&q, 0f64, 1f64));
+
+            let q = Phasor { mag: -mag, tan };
+            assert!(!p.relative_eq(&q, 0f64, 1f64));
+
+            let q = Phasor { mag: -mag, tan: -tan };
+            assert!(p.relative_eq(&q, 0f64, EPSILON));
+        }
+
+        #[test]
+        fn relative_eq_scaled(mag in regular(), tan in not_nan()) {
+            let p = Phasor { mag: mag * 1.000001, tan };
+            let q = Phasor { mag: mag / 1.000001, tan };
+
+            assert!(p.relative_eq(&q, 0f64, 1E-10));
             assert!(!p.relative_eq(&q, 0f64, 1E-12));
         }
 
         #[test]
-        fn ulps_eq(a: f64, b: f64) {
-            let p = Phasor::rect(a * 1.000001, b * 0.999999);
-            let q = Phasor::rect(a * 0.999999, b * 1.000001);
+        fn relative_eq_opposite(mag in nonzero(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            let q = Phasor { mag: -mag, tan };
 
-            assert!(p.ulps_eq(&q, 0f64, 100000));
+            assert!(!p.relative_eq(&q, 0f64, 1f64));
         }
 
         #[test]
-        fn ulps_neq(a in NORMAL, b in NORMAL) {
-            let p = Phasor::rect(a * 1.000001, b * 0.999999);
-            let q = Phasor::rect(a * 0.999999, b * 1.000001);
+        fn ulps_eq_nan(nan in nan(), not_nan in not_nan()) {
+            let p = Phasor { mag: nan, tan: nan };
+            assert!(!p.ulps_eq(&p, 0f64, u32::MAX));
 
+            let p = Phasor { mag: nan, tan: not_nan };
+            assert!(!p.ulps_eq(&p, 0f64, u32::MAX));
+
+            let p = Phasor { mag: not_nan, tan: nan };
+            assert!(!p.ulps_eq(&p, 0f64, u32::MAX));
+        }
+
+        #[test]
+        fn ulps_eq_same(mag in not_nan(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            assert!(p.ulps_eq(&p, 0f64, 2));
+        }
+
+        #[test]
+        fn ulps_eq_real(mag in nonzero(), tan in zero()) {
+            let p = Phasor { mag, tan };
+            assert!(p.ulps_eq(&p, 0f64, 2));
+
+            let q = Phasor { mag, tan: -tan };
+            assert!(p.ulps_eq(&q, 0f64, 2));
+
+            let q = Phasor { mag: -mag, tan };
+            assert!(!p.ulps_eq(&q, 0f64, u32::MAX));
+
+            let q = Phasor { mag: -mag, tan: -tan };
+            assert!(!p.ulps_eq(&q, 0f64, u32::MAX));
+        }
+
+        #[test]
+        fn ulps_eq_imaginary(mag in nonzero(), tan in infinite()) {
+            let p = Phasor { mag, tan };
+            assert!(p.ulps_eq(&p, 0f64, 2));
+
+            let q = Phasor { mag, tan: -tan };
+            assert!(!p.ulps_eq(&q, 0f64, u32::MAX));
+
+            let q = Phasor { mag: -mag, tan };
+            assert!(!p.ulps_eq(&q, 0f64, u32::MAX));
+
+            let q = Phasor { mag: -mag, tan: -tan };
+            assert!(p.ulps_eq(&q, 0f64, 2));
+        }
+
+        #[test]
+        fn ulps_eq_scaled(mag in regular(), tan in not_nan()) {
+            let p = Phasor { mag: mag * 1.000001, tan };
+            let q = Phasor { mag: mag / 1.000001, tan };
+
+            assert!(p.ulps_eq(&q, 0f64, 100000));
             assert!(!p.ulps_eq(&q, 0f64, 1000));
+        }
+
+        #[test]
+        fn ulps_eq_opposite(mag in nonzero(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            let q = Phasor { mag: -mag, tan };
+
+            assert!(!p.ulps_eq(&q, 0f64, u32::MAX));
         }
     }
 }

src/trig.rs
@@ -1,77 +1,142 @@
+use core::f64::consts::FRAC_1_SQRT_2;
+use core::num::FpCategory::{Infinite, Zero};
+
+#[inline(always)]
+pub(crate) fn sinatan(x: f64) -> f64 {
+    sinatan2(x, 1f64)
+}
+
+#[inline(always)]
 pub(crate) fn cosatan(x: f64) -> f64 {
-    if x.is_nan() {
-        x
-    } else if x.abs() <= 1f64 {
-        x.hypot(1f64).recip()
-    } else {
-        sinatan(x.recip()).abs()
+    cosatan2(x, 1f64)
+}
+
+pub(crate) fn sinatan2(s: f64, c: f64) -> f64 {
+    match (s.classify(), c.classify()) {
+        (Zero, Zero) => s,
+        (Infinite, Infinite) => FRAC_1_SQRT_2.copysign(s),
+        _ => 1f64.hypot(c / s).recip().copysign(s),
     }
 }
 
-pub(crate) fn sinatan(x: f64) -> f64 {
-    if x.is_nan() {
-        x
-    } else if x.abs() <= 1f64 {
-        x / x.hypot(1f64)
-    } else {
-        cosatan(x.recip()).copysign(x)
+pub(crate) fn cosatan2(s: f64, c: f64) -> f64 {
+    match (s.classify(), c.classify()) {
+        (Zero, Zero) => c.signum(),
+        (Infinite, Infinite) => FRAC_1_SQRT_2.copysign(c),
+        _ => 1f64.hypot(s / c).recip().copysign(c),
     }
 }
 
 pub(crate) fn tanaddatan(x: f64, y: f64) -> f64 {
-    if x.abs() <= 1f64 && y.abs() <= 1f64 {
-        (x + y) / x.mul_add(-y, 1f64)
-    } else if x.abs() > 1f64 && y.abs() <= 1f64 {
-        y.mul_add(x.recip(), 1f64) / (x.recip() - y)
-    } else if x.abs() <= 1f64 && y.abs() > 1f64 {
-        x.mul_add(y.recip(), 1f64) / (y.recip() - x)
-    } else {
-        (x.recip() + y.recip()) / x.recip().mul_add(y.recip(), -1f64)
+    match (x.abs() <= 1f64, y.abs() <= 1f64) {
+        (true, true) => (x + y) / x.mul_add(-y, 1f64),
+        (false, true) => y.mul_add(x.recip(), 1f64) / (x.recip() - y),
+        (true, false) => x.mul_add(y.recip(), 1f64) / (y.recip() - x),
+        (false, false) => (x.recip() + y.recip()) / x.recip().mul_add(y.recip(), -1f64),
     }
 }
 
 pub(crate) fn tansubatan(x: f64, y: f64) -> f64 {
-    if x.abs() <= 1f64 && y.abs() <= 1f64 {
-        (x - y) / x.mul_add(y, 1f64)
-    } else if x.abs() > 1f64 && y.abs() <= 1f64 {
-        y.mul_add(-x.recip(), 1f64) / (x.recip() + y)
-    } else if x.abs() <= 1f64 && y.abs() > 1f64 {
-        x.mul_add(y.recip(), -1f64) / (y.recip() + x)
+    match (x.abs() <= 1f64, y.abs() <= 1f64) {
+        (true, true) => (x - y) / x.mul_add(y, 1f64),
+        (false, true) => y.mul_add(-x.recip(), 1f64) / (x.recip() + y),
+        (true, false) => x.mul_add(y.recip(), -1f64) / (y.recip() + x),
+        (false, false) => (y.recip() - x.recip()) / x.recip().mul_add(y.recip(), 1f64),
+    }
+}
+
+pub(crate) fn cossubatan(x: f64, y: f64) -> f64 {
+    let tan = tansubatan(x, y);
+
+    if x.signum() != tan.signum() && y.signum() == tan.signum() {
+        -cosatan(tan)
     } else {
-        (y.recip() - x.recip()) / x.recip().mul_add(y.recip(), 1f64)
+        cosatan(tan)
     }
 }
 
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
     use super::*;
+    use crate::arbitrary::{any, *};
     use crate::assert_close_to;
-    use core::f64::consts::PI;
+    use core::f64::consts::FRAC_PI_2;
     use proptest::prelude::*;
 
     proptest! {
         #[test]
-        fn cosatan_equals_cosine_of_arc_tangent(x: f64) {
-            assert_close_to!(cosatan(x), x.atan().cos());
-        }
-
-        #[test]
-        fn sinatan_equals_sine_of_arc_tangent(x: f64) {
+        fn sinatan_equals_sine_of_atan(x in not_nan()) {
             assert_close_to!(sinatan(x), x.atan().sin());
         }
 
         #[test]
-        fn tanaddatan_equals_tangent_of_the_sum_of_two_arc_tangent(mut x: f64, mut y: f64) {
-            x %= 2f64 * PI;
-            y %= 2f64 * PI;
-            assert_close_to!(tanaddatan(x.tan(), y.tan()), (x + y).tan(), tol = 1E-9);
+        fn sinatan_of_nan_is_nan(x in nan()) {
+            assert!(sinatan(x).is_nan());
         }
 
         #[test]
-        fn tansubatan_equals_tangent_of_the_difference_of_two_arc_tangent(mut x: f64, mut y: f64) {
-            x %= 2f64 * PI;
-            y %= 2f64 * PI;
-            assert_close_to!(tansubatan(x.tan(), y.tan()), (x - y).tan(), tol = 1E-9);
+        fn cosatan_equals_cosine_of_atan(x in not_nan()) {
+            assert_close_to!(cosatan(x), x.atan().cos());
+        }
+
+        #[test]
+        fn cosatan_of_nan_is_nan(x in nan()) {
+            assert!(cosatan(x).is_nan());
+        }
+
+        #[test]
+        fn sinatan2_equals_sine_of_atan2(x in not_nan(), y in not_nan()) {
+            assert_close_to!(sinatan2(x, y), x.atan2(y).sin());
+        }
+
+        #[test]
+        fn sinatan2_of_nan_is_nan(x in nan(), y in any()) {
+            assert!(sinatan2(x, y).is_nan());
+            assert!(sinatan2(y, x).is_nan());
+        }
+
+        #[test]
+        fn cosatan2_equals_cosine_of_atan2(x in not_nan(), y in not_nan()) {
+            assert_close_to!(cosatan2(x, y), x.atan2(y).cos());
+        }
+
+        #[test]
+        fn cosatan2_of_nan_is_nan(x in nan(), y in any()) {
+            assert!(cosatan2(x, y).is_nan());
+            assert!(cosatan2(y, x).is_nan());
+        }
+
+        #[test]
+        fn tanaddatan_equals_tangent_of_the_sum_of_atan(x in modulo(FRAC_PI_2), y in modulo(FRAC_PI_2)) {
+            assert_close_to!(tanaddatan(x.tan(), y.tan()), (x + y).tan(), tol = 1E-10);
+        }
+
+        #[test]
+        fn tanaddatan_of_nan_is_nan(x in nan(), y in any()) {
+            assert!(tanaddatan(x, y).is_nan());
+            assert!(tanaddatan(y, x).is_nan());
+        }
+
+        #[test]
+        fn tansubatan_equals_tangent_of_the_difference_of_atan(x in modulo(FRAC_PI_2), y in modulo(FRAC_PI_2)) {
+            assert_close_to!(tansubatan(x.tan(), y.tan()), (x - y).tan(), tol = 1E-10);
+        }
+
+        #[test]
+        fn tansubatan_of_nan_is_nan(x in nan(), y in any()) {
+            assert!(tansubatan(x, y).is_nan());
+            assert!(tansubatan(y, x).is_nan());
+        }
+
+        #[test]
+        fn cossubatan_equals_cosine_of_the_difference_of_atan(x in modulo(FRAC_PI_2), y in modulo(FRAC_PI_2)) {
+            assert_close_to!(cossubatan(x.tan(), y.tan()), (x - y).cos());
+        }
+
+        #[test]
+        fn cossubatan_of_nan_is_nan(x in nan(), y in any()) {
+            assert!(cossubatan(x, y).is_nan());
+            assert!(cossubatan(y, x).is_nan());
         }
     }
 }