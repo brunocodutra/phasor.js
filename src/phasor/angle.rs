6b5d5ba9671c0d7649a258791a3b1f66feb0e452
Bruno Dutra <brunocodutra@gmail.com>
2020-09-22 00:33:39 +0200

    cover edge cases in angle()

--- src/phasor/angle.rs
+++ src/phasor/angle.rs
@@ -1,5 +1,5 @@
 use crate::Phasor;
-use core::f64::consts::PI;
+use core::f64::{consts::PI, NAN};
 
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
@@ -8,7 +8,9 @@ use wasm_bindgen::prelude::*;
 impl Phasor {
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
     pub fn angle(&self) -> f64 {
-        if self.mag.is_sign_positive() {
+        if self.is_nan() {
+            NAN
+        } else if self.mag.is_sign_positive() {
             self.tan.atan()
         } else {
             self.tan.atan() - PI.copysign(self.tan)
@@ -19,15 +21,33 @@ impl Phasor {
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
     use super::*;
+    use crate::arbitrary::{any, *};
     use crate::assert_close_to;
     use proptest::prelude::*;
 
     proptest! {
         #[test]
-        fn angle(mag: f64, ang: f64) {
-            let p = Phasor::polar(mag, ang);
-            assert_close_to!(p.angle().cos(), ang.cos() * mag.signum());
-            assert_close_to!(p.angle().sin(), ang.sin() * mag.signum());
+        fn the_modulus_of_the_angle_of_a_phasor_is_never_greater_than_pi(mag in not_nan(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            assert!(!(p.angle().abs() > PI));
+        }
+
+        #[test]
+        fn the_angle_of_a_finite_nonzero_phasor_equals_the_arc_whose_tangent_is_imaginary_part_over_real_part(mag in regular(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            assert_close_to!(p.angle(), p.imag().atan2(p.real()), tol = 1E-12);
+        }
+
+        #[test]
+        fn the_angle_of_a_phasor_with_undefined_magnitude_is_nan(mag in nan(), tan in any()) {
+            let p = Phasor { mag, tan };
+            assert!(p.angle().is_nan());
+        }
+
+        #[test]
+        fn the_angle_of_a_phasor_with_undefined_tangent_is_nan(mag in any(), tan in nan()) {
+            let p = Phasor { mag, tan };
+            assert!(p.angle().is_nan());
         }
     }
 }
