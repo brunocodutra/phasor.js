55ca0cd9c18aef1cec59383ffdcf843c70331acf
Bruno Dutra <brunocodutra@gmail.com>
2020-09-26 20:00:31 +0200

    improve test coverage for polar() and rect()

--- src/phasor/polar.rs
+++ src/phasor/polar.rs
@@ -13,3 +13,49 @@ impl Phasor {
         }
     }
 }
+
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod tests {
+    use super::*;
+    use crate::arbitrary::{any, *};
+    use crate::assert_close_to;
+    use proptest::prelude::*;
+
+    proptest! {
+        #[test]
+        fn polar_form_preserves_modulus_of_magnitude(mag in not_nan(), ang in finite()) {
+            let p = Phasor::polar(mag, ang);
+            assert_close_to!(p.norm(), mag.abs());
+        }
+
+        #[test]
+        fn polar_form_preserves_angle_if_magnitude_is_positive(mag in not_nan(), ang in finite()) {
+            let p = Phasor::polar(mag.abs(), ang);
+            assert_close_to!(p.angle(), ang.sin().atan2(ang.cos()));
+        }
+
+        #[test]
+        fn polar_form_with_zero_angle_is_purely_real(mag in not_nan(), ang in zero()) {
+            let p = Phasor::polar(mag, ang);
+            assert!(p.is_real());
+        }
+
+        #[test]
+        fn polar_form_with_infinite_angle_is_nan(mag in not_nan(), ang in infinite()) {
+            let p = Phasor::polar(mag, ang);
+            assert!(p.is_nan());
+        }
+
+        #[test]
+        fn polar_form_with_undefined_magnitude_is_nan(mag in nan(), ang in any()) {
+            let p = Phasor::polar(mag, ang);
+            assert!(p.is_nan());
+        }
+
+        #[test]
+        fn polar_form_with_undefined_angle_is_nan(mag in any(), ang in nan()) {
+            let p = Phasor::polar(mag, ang);
+            assert!(p.is_nan());
+        }
+    }
+}
