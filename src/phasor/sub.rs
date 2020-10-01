be78ec8b2be9e8e8a1ffb305597352b51c4a04a6
Bruno Dutra <brunocodutra@gmail.com>
2020-10-02 00:04:27 +0200

    fix subtle bug in Add for Phasor

--- src/phasor/sub.rs
+++ src/phasor/sub.rs
@@ -13,7 +13,8 @@ impl Sub for Phasor {
 mod tests {
     use super::*;
     use crate::arbitrary::{any, *};
-    use crate::{assert::ulps_or_relative_eq, assert_close_to, trig::cossubatan};
+    use crate::trig::{cossubatan, tanaddatan};
+    use crate::{assert::ulps_or_relative_eq, assert_close_to};
     use alloc::format;
     use core::f64::consts::{FRAC_PI_2, SQRT_2};
     use proptest::prelude::*;
@@ -48,6 +49,83 @@ mod tests {
         }
 
         #[test]
+        fn subtracting_phasors_with_equal_magnitudes_has_tangent_angle(mag in not_nan(), t in not_nan(), u in not_nan()) {
+            prop_assume!(!mag.is_infinite() || cossubatan(t, u) != 1f64);
+
+            let p = Phasor { mag, tan: t };
+            let q = Phasor { mag, tan: u };
+
+            let (s, c) = tanaddatan(t, u);
+
+            let r = Phasor::polar(
+                mag * (1f64 - cossubatan(t, u)).sqrt() * SQRT_2,
+                s.atan2(c) / 2f64 + if t < u { -FRAC_PI_2 } else { FRAC_PI_2 },
+            );
+
+            if ulps_or_relative_eq(&p, &q, 0f64) {
+                assert_close_to!(p - q, -q + p);
+            } else {
+                assert_close_to!(p - q, r);
+                assert_close_to!(q - p, -r);
+            }
+        }
+
+        #[test]
+        fn subtracting_phasors_with_opposite_magnitudes_has_tangent_angle(mag in not_nan(), t in not_nan(), u in not_nan()) {
+            prop_assume!(!mag.is_infinite() || cossubatan(t, u) != -1f64);
+
+            let p = Phasor { mag, tan: t };
+            let q = Phasor { mag: -mag, tan: u };
+
+            let (s, c) = tanaddatan(t, u);
+
+            let r = Phasor::polar(
+                mag * (1f64 + cossubatan(t, u)).sqrt() * SQRT_2,
+                s.atan2(c) / 2f64,
+            );
+
+            assert_close_to!(p - q, r);
+            assert_close_to!(q - p, -r);
+        }
+
+        #[test]
+        fn subtracting_conjugate_finite_phasors_is_purely_imaginary(mag in not_nan(), tan in not_nan()) {
+            prop_assume!(!mag.is_infinite() || cossubatan(tan, -tan) != 1f64);
+
+            let p = Phasor { mag, tan };
+            let q = p.conj();
+            let r = Phasor { mag: mag * (1f64 - cossubatan(tan, -tan)).sqrt() * SQRT_2, tan: f64::INFINITY.copysign(tan) };
+
+            assert_close_to!(p - q, r);
+            assert_close_to!(q - p, -r);
+        }
+
+        #[test]
+        fn subtracting_equal_finite_phasors_has_zero_magnitude_and_orthogonal_angle(mag in finite(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            let r = Phasor { mag: 0f64, tan: -tan.recip() };
+
+            assert_close_to!(p - p, r);
+        }
+
+        #[test]
+        fn subtracting_opposite_phasors_has_double_magnitude(mag in not_nan(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+            let q = -p;
+            let r = Phasor { mag: 2f64 * mag, tan };
+
+            assert_close_to!(p - q, r);
+            assert_close_to!(q - p, -r);
+        }
+
+        #[test]
+        fn subtracting_equal_infinite_phasors_is_nan(mag in infinite(), tan in not_nan()) {
+            let p = Phasor { mag, tan };
+
+            assert!((p - p).is_nan());
+        }
+
+        #[test]
         fn subtracting_phasor_that_has_undefined_magnitude_is_nan(a in any(), b in any(), c in nan(), d in any()) {
             let p = Phasor { mag: a, tan: b };
             let q = Phasor { mag: c, tan: d };
@@ -63,55 +141,6 @@ mod tests {
 
             assert!((p - q).is_nan());
             assert!((q - p).is_nan());
-        }
-
-        #[test]
-        fn subtracting_phasors_with_the_same_magnitude_has_tangent_angle(m in not_nan(), t in not_nan(), u in not_nan()) {
-            let p = Phasor { mag: m, tan: t };
-            let q = Phasor { mag: m, tan: u };
-
-            prop_assume!(!ulps_or_relative_eq(&p, &q, 0f64));
-
-            let r = Phasor::polar(
-                m * (1f64 - cossubatan(t, u)).sqrt() * SQRT_2,
-                (t.atan() + u.atan()) / 2f64 + if t > u { FRAC_PI_2 } else { -FRAC_PI_2 },
-            );
-
-            assert_close_to!(p - q, r);
-            assert_close_to!(q - p, -r);
-        }
-
-        #[test]
-        fn subtracting_opposite_phasors_has_double_magnitude(a in not_nan(), b in not_nan()) {
-            let p = Phasor { mag: a, tan: b };
-            let q = -p;
-            let r = Phasor { mag: 2f64 * a, tan: b };
-
-            assert_close_to!(p - q, r);
-        }
-
-        #[test]
-        fn subtracting_equal_finite_phasors_has_zero_magnitude_and_orthogonal_angle(a in finite(), b in not_nan()) {
-            let p = Phasor { mag: a, tan: b };
-            let r = Phasor { mag: 0f64, tan: -b.recip() };
-
-            assert_close_to!(p - p, r);
-        }
-
-        #[test]
-        fn subtracting_finite_phasor_to_its_conjugate_is_purely_imaginary(a in finite(), b in not_nan()) {
-            let p = Phasor { mag: a, tan: b };
-            let q = p.conj();
-
-            assert!((p - q).is_imaginary());
-            assert!((q - p).is_imaginary());
-        }
-
-        #[test]
-        fn subtracting_equal_infinite_phasors_is_nan(a in infinite(), b in not_nan()) {
-            let p = Phasor { mag: a, tan: b };
-
-            assert!((p - p).is_nan());
         }
     }
 }
