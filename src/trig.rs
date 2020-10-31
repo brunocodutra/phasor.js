03bb10e87e71810ecc8af33dd8393f50bf33b79c
Bruno Dutra <brunocodutra@gmail.com>
2020-10-31 13:10:43 +0100

    turn cossubatan() into a test utility

--- src/trig.rs
+++ src/trig.rs
@@ -40,6 +40,7 @@ pub(crate) fn tansubatan(x: f64, y: f64) -> (f64, f64) {
     (xs * yc - xc * ys, xs * ys + xc * yc)
 }
 
+#[cfg(all(test, not(target_arch = "wasm32")))]
 pub(crate) fn cossubatan(x: f64, y: f64) -> f64 {
     let (s, c) = tansubatan(x, y);
     cosatan2(s, c)
