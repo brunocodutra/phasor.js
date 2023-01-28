b7a5ebe5849a0b6827f51b63bb431cbe596ec651
Bruno Dutra <brunocodutra@gmail.com>
2023-01-28 19:14:10 +0100

    fix newest clippy warnings

--- src/phasor/display.rs
+++ src/phasor/display.rs
@@ -26,22 +26,22 @@ mod tests {
 
     #[proptest]
     fn starts_with_the_norm(p: Phasor) {
-        assert!(format!("{}", p).starts_with(&format!("{}", p.norm())));
-        assert!(format!("{:e}", p).starts_with(&format!("{:e}", p.norm())));
-        assert!(format!("{:E}", p).starts_with(&format!("{:E}", p.norm())));
+        assert!(format!("{p}").starts_with(&format!("{}", p.norm())));
+        assert!(format!("{p:e}").starts_with(&format!("{:e}", p.norm())));
+        assert!(format!("{p:E}").starts_with(&format!("{:E}", p.norm())));
     }
 
     #[proptest]
     fn ends_with_the_angle(p: Phasor) {
-        assert!(format!("{}", p).ends_with(&format!("{}", p.angle())));
-        assert!(format!("{:e}", p).ends_with(&format!("{:e}", p.angle())));
-        assert!(format!("{:E}", p).ends_with(&format!("{:E}", p.angle())));
+        assert!(format!("{p}").ends_with(&format!("{}", p.angle())));
+        assert!(format!("{p:e}").ends_with(&format!("{:e}", p.angle())));
+        assert!(format!("{p:E}").ends_with(&format!("{:E}", p.angle())));
     }
 
     #[proptest]
     fn connects_norm_and_angle_by_the_unicode_angle_symbol(p: Phasor) {
-        assert!(format!("{}", p).contains('∠'));
-        assert!(format!("{:e}", p).contains('∠'));
-        assert!(format!("{:E}", p).contains('∠'));
+        assert!(format!("{p}").contains('∠'));
+        assert!(format!("{p:e}").contains('∠'));
+        assert!(format!("{p:E}").contains('∠'));
     }
 }
